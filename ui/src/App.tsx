b315e8de57ea25422f96bc2dde5d7757275b6609
Jamie Strange <jamiestrange@BEL-MC-211009A.local>
2022-11-24 14:41:05 +0000

    Creates basic login and returns token

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
 
@@ -14,12 +15,46 @@ function App() {
     setPassword(event.target.value);
   };
 
+  const instance = axios.create({
+    baseURL: "http://localhost:3001/",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const login = async (email: string, password: string) => {
+    return await instance
+      .post("/auth", { email, password })
+      .then((response) => {
+        if (response.data.accessToken) {
+          console.log(response.data);
+        }
+        email = '';
+        password = '';
+        //window.location.reload();
+        setUser(response.data);
+        return response.data;
+      });
+  };
+
+  const setUser = (user: any) => {
+    localStorage.setItem("user", JSON.stringify(user));
+  };
+
+  const handleLogin = async () => {
+    try {
+      login(email, password);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
       <div>
         <input placeholder='Enter Email Address' value={email} onChange={handleEmailChange}></input>
         <input placeholder='Enter Password' value={password} onChange={handlePasswordChange}></input>
-        <button type='submit'>Login</button>
+        <button type='submit' onClick={handleLogin}>Login</button>
       </div>
     </div>
   );
